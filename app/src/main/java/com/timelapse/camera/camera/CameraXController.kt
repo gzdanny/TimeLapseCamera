41ec73047726a64c44ada79c71202080df0cce4e
Danny0GZ <Danny@GZ.Desk>
2026-08-25 23:01:16 +0800

    update: build.gradle versionName 同步为 0.9.4

--- app/src/main/java/com/timelapse/camera/camera/CameraXController.kt
+++ app/src/main/java/com/timelapse/camera/camera/CameraXController.kt
@@ -7,7 +7,6 @@ import android.graphics.ImageFormat
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
 import android.util.Size
-import android.view.Surface
 import android.view.WindowManager
 import androidx.camera.camera2.interop.Camera2CameraInfo
 import androidx.camera.camera2.interop.ExperimentalCamera2Interop
